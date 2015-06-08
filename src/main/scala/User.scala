3da3fdf4a2f2b51f37691703394d413d913eb658
pyanzin <igor.pyanzin@gmail.com>
2015-06-08 18:33:02 +0300

    Added some fields to User class

--- src/main/scala/User.scala
+++ src/main/scala/User.scala
@@ -3,5 +3,8 @@ package uds
 case class User(
   id: Long,
   firstName: String,
-  lastName: String
+  lastName: String,
+  gender: Option[Boolean],
+  birthDate: Option[String],
+  cityId: Option[Int]
 )
--- src/main/scala/Vk.scala
+++ src/main/scala/Vk.scala
@@ -8,7 +8,20 @@ package object uds
     def toUser(x: JValue) = {
         User(x \ "uid" match { case JInt(x) => x.toLong },
           x \ "first_name" match { case JString(x) => x },
-          x \ "last_name" match { case JString(x) => x }
+          x \ "last_name" match { case JString(x) => x },
+          x \ "sex" match {
+            case JInt(s) if s == 1 => Some(false)
+            case JInt(s) if s == 2 => Some(true)
+            case _ => None
+          },
+          x \ "bdate" match {
+            case JString(x) => Some(x)
+            case _ => None
+          },
+          x \ "city" match {
+            case JInt(x) => Some(x.toInt)
+            case _ => None
+          }
         )
     }
 
@@ -32,14 +45,17 @@ package object uds
             (("access_token" -> token) :: params.toList): _* )
 
       def getUsers(ids: Long *) = {
-        Thread.sleep(1000)
-        val f = vkMethod("users.get", "user_ids" -> ids.mkString(","))
+        Thread.sleep(400)
+        val f = vkMethod("users.get", 
+          "user_ids" -> ids.mkString(","),
+          "fields" -> List("bdate", "city", "sex").mkString(",")
+        )
         val JArray(usersJson) = parse(f) \\ "response" 
         usersJson map toUser
       }
 
       def getFriends(id: Long) = {
-        Thread.sleep(1000)
+        Thread.sleep(400)
         val f = vkMethod("friends.get", "user_id" -> id.toString)
         (parse(f) \\ "response").extract[List[Long]]
       }
