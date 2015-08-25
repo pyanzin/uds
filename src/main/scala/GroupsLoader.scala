070b210eb710eb58054c82b494f59b7a29cfbb97
unknown <Igor.Pyanzin@aw003.astro.local>
2015-08-25 18:24:28 +0300

    fixed GroupsLoader crash

--- src/main/scala/GroupsLoader.scala
+++ src/main/scala/GroupsLoader.scala
@@ -18,28 +18,29 @@ case class Group(
 ) extends VkNode
 
 class GroupsLoader(vk: Vk) extends Block[Graph[VkNode, LUnDiEdge], Graph[VkNode, LUnDiEdge]] {
-  def toGroup(x: JValue): Option[Group] = x match {
-    case JObject => Some(Group(
-      x \ "gid" match {case JInt(x) => x.toLong },
-      x \ "name" match { case JString(x) => x },
-      x \ "screen_name" match { case JString(x) => x },
-      x \ "is_closed" match {
-        case JInt(x) if x==1 => true
-        case _ => false
-      },
-      x \ "type" match {
-        case JString("group") => false
-        case _ => true
-      }
-    ))
-    case _ => None
-  }
+  def toGroup(x: JValue) = Group(
+    x \ "gid" match {case JInt(x) => x.toLong },
+    x \ "name" match { case JString(x) => x },
+    x \ "screen_name" match { case JString(x) => x },
+    x \ "is_closed" match {
+      case JInt(x) if x==1 => true
+      case _ => false
+    },
+    x \ "type" match {
+      case JString("group") => false
+      case _ => true
+    }
+  )
+  
 
   def getGroups(userId: Long) = {
     val json = vk.vkMethod("groups.get", "user_id" -> userId.toString, "extended" -> "1")
-    val JArray(groupsJson) = parse(json) \\ "response"
-    groupsJson flatMap toGroup
+    parse(json) \\ "response" match {
+      case JArray(groupsJson) => groupsJson.tail map toGroup
+      case _ => List()
+    }    
   }
+
   def apply(g: Graph[VkNode, LUnDiEdge]): Graph[VkNode, LUnDiEdge] = {
     val users = g.nodes.map(_.value).collect{ case u: User => u }
     val groupsRels = users flatMap { u =>
