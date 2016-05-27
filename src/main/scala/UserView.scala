9a7f51fe903c8d595185ef54d3c31d9cdff3584a
Igor Pyanzin <igor.pyanzin@gmail.com>
2016-05-27 18:00:05 +0300

    Some small method

--- src/main/scala/UserView.scala
+++ src/main/scala/UserView.scala
@@ -1,3 +1,30 @@
 package uds
 
-case class UserView(uid: Long, first_name: String, last_name: String, sex: Int, bdate: String, city: Long, screen_name: String, photo_200: String)
+trait View[T] {
+	def convert: T
+}
+
+case class UserView (
+	uid: Int,
+	first_name: String,
+	last_name: String,
+	sex: Int,
+	bdate: Some[String],
+	city: Some[Int],
+	photo_100: String,
+	deactivated: Some[String]
+) extends View[User] {
+	def convert = User(
+		uid,
+		first_name,
+		last_name,
+		sex match {
+			case 0 => None 
+			case 1 => Some(false)
+			case 2 => Some(true)
+		},
+		bdate,
+		city,
+		photo_100
+	)
+}
