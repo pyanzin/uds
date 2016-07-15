be8efc98d6fb6826346440c7f260fdbb567374e1
Igor Pyanzin <igor.pyanzin@gmail.com>
2016-07-15 17:28:08 +0300

    VkActor has been added

--- src/main/scala/ASystem.scala
+++ src/main/scala/ASystem.scala
@@ -9,6 +9,9 @@ import akka.http.scaladsl.model._
 import scala.concurrent.Future
 import scala.util.{ Failure, Success }
 
+import dispatch._, Defaults._
+import net.liftweb.json._
+import scala.concurrent.ExecutionContext.Implicits.global._
 
 import uds._
 import uds.graph._
@@ -27,13 +30,71 @@ abstract class BaseAnalysisActor extends Actor {
 	def notificator = context.actorSelection("/user/notificator")
 }
 
-class InitialAnalysisActor extends Actor {
+class InitialAnalysisActor(targetId: Long, clientId: Long) extends BaseAnalysisActor {
+
+	val vk = context.actorOf(Props(new VkActor))
+
+	vk ! FriendsRequest(targetId)
+
 	def receive = {
-		case ar: AnalysisRequest => {
-			val vk = new Vk("")
-	    	val loader = new InitialLoader(vk)
-	    	sender ! loader(ar.targetId)
+//		case ar: AnalysisRequest => {
+//			val vk = new Vk("")
+//	    	val loader = new InitialLoader(vk)
+//	    	sender ! loader(ar.targetId)
+//		}
+
+		case users: Seq[User] => {
+
 		}
+
+		case (id: Long, friendIds: Seq[Long]) => {
+
+		}
+	}
+
+	def name = s"initial-$targetId-$clientId"
+}
+
+class VkActor extends Actor {
+	def receive = {
+		case request: VkRequest[_] => {
+			implicit val materializer = akka.stream.ActorMaterializer()
+
+			val future: Future[HttpResponse] = Http(context.system).singleRequest(HttpRequest(uri = request.uri))
+
+			future.onComplete {
+				case Success(x) => sender ! (request.convert(x.toString))				
+			}
+		}
+	}
+}
+
+abstract class VkRequest[T] {
+	def uri: String 
+	def convert(s: String): T
+}
+
+case class UsersRequest(ids: Long*) extends VkRequest[Seq[User]] {
+	implicit val formats = DefaultFormats
+    import net.liftweb.json.JsonParser._
+
+	val uri = "https://api.vk.com/method/users.get?user_ids=" + ids.mkString(",")
+
+	def convert(s: String): Seq[User] = {
+		val usersJson = parse(s) \\ "response" 
+        usersJson.extract[List[User]]
+	}
+}
+
+case class FriendsRequest(id: Long) extends VkRequest[(Long, Seq[Long])] {
+	implicit val formats = DefaultFormats
+    import net.liftweb.json.JsonParser._
+
+	val uri = "https://api.vk.com/method/friends.get?user_id=" + id
+
+	def convert(s: String): (Long, Seq[Long]) = {
+		val usersJson = parse(s) \\ "response" 
+        id -> usersJson.extract[List[Long]]
 	}
 }
 
